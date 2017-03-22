9bfebe5ad2a855270caff4043aff7e621aa130b3
Rémi M <remi.marandon@gmail.com>
2017-03-22 23:18:53 +0100

    [+] MessageControler : traitements des message (MainController ? on s'en fou pour le moment, ça fait tt)

--- src/main/java/utils/MessageControler.java
+++ src/main/java/utils/MessageControler.java
@@ -1,14 +1,20 @@
 package utils;
 
 import java.util.ArrayList;
-import java.util.regex.Pattern;
 
 import org.apache.commons.validator.routines.InetAddressValidator;
+
+import serial.MessageFromServer;
+import serial.MessageToServer;
 
 public class MessageControler {
 		
 	public MessageControler() {
 		// do nothing
+	}
+	
+	public void process(String s) {
+		MessageToServer msg = new MessageToServer(s);
 	}
 
 	// Function to call if Message is a command
@@ -22,12 +28,21 @@ public class MessageControler {
 				
 				//check if args are OK
 				boolean isValidIP = this.isValidIpAddress(serverIP);
-				boolean isNickname = this.isNickname(nickname);
+				boolean isValidNickname = this.isValidNickname(nickname);
 				
 				// if not maybe in wrong order?
 				
 				//if ok try to connect
-				this.connectToServer(serverIP, nickname);
+				if (isValidIP && isValidNickname) {
+					try {
+						this.connectToServer(serverIP, nickname);
+					} catch (ConnectionHandlerException e) {
+						throw new MessageControlerException(
+								"An error occured attempting to connect to IP \""
+										+ serverIP + "\" with nickname \""
+										+ nickname + "\".", e);
+					}
+				}
 				
 			} else {
 				throw new MessageControlerException("#CONNECT expects two args, the target server ip and a nickname.");
@@ -40,7 +55,13 @@ public class MessageControler {
 				//check if args are OK
 				
 				//if ok try to connect
-				this.connectToChannel(channel);
+				try {
+					this.connectToChannel(channel);
+				} catch (ConnectionHandlerException e) {
+					// TODO Auto-generated catch block
+					throw new MessageControlerException(
+							"Unabled to join channel \"" + channel + "\".", e);
+				}
 				
 			} else {
 				throw new MessageControlerException("#JOIN expects 1 args, the target channel name.");
@@ -55,24 +76,62 @@ public class MessageControler {
 		}
 	}
 
+	// TODO nothing to do here
 	private boolean isValidIpAddress(String serverIP) {
 		InetAddressValidator validator = new InetAddressValidator();	
 		return (validator.isValidInet4Address(serverIP) || validator.isValidInet6Address(serverIP)); 
 	}
 
-	private boolean isNickname(String nickname) {
+	private boolean isValidNickname(String nickname) {
 		// String de + de 3 caractères
 		return nickname.length() > 3;
 	}
 
-	private void connectToServer(String serverIP, String nickname) throws MessageControlerException {
-		// gère la connexion au serveur
-		// -> appel une methode du package io ?
+	private void connectToServer(String serverIP, String nickname) throws ConnectionHandlerException {
+		// connect to server
+		// TODO singleton getInstance + check isOpened connection
+		ConnectionHandler handler = new ConnectionHandler();
+		handler.openConnection(serverIP);
+		
+		// init MSG
+		ArrayList<String> args = new ArrayList<String>();
+		args.add(serverIP);
+		args.add(nickname);
+		MessageToServer msg = new MessageToServer(nickname, "#connect", args);
+		// write msg
+		handler.write(msg.toString());
+		
+		// read response -> wait x second, then timeout
+		// is username ok etc...
+		String s = handler.read();
+		this.processServerMessage(s);
 		
 	}
 	
-	private void connectToChannel(String channel) {
-		// TODO Auto-generated method stub
+	private void processServerMessage(String s) {
+
+		// init server message
+		MessageFromServer msgFromServer = new MessageFromServer(s);
 		
+		// check is valid ?
+		boolean isValid = msgFromServer.isValid();
+		if (! isValid) {
+			
+		}
+		// server or user ?
+		boolean isFromServer = msgFromServer.isFromServer();
+		// case 1  Server
+		if (isFromServer) {
+			
+		}
+		// case 2 User
+		else {
+			
+		}
+	}
+
+	private void connectToChannel(String channel) throws ConnectionHandlerException {
+		// TODO Auto-generated method stub
+		ConnectionHandler handler = new ConnectionHandler();
 	}
 }
