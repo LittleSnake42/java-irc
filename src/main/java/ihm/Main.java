43036d95c1d99e5ed3ca8f109312a9b8b631fef0
Macklegan <michael.bussiere42@gmail.com>
2017-03-26 19:25:00 +0200

    [*] Modification des classes Main et Window
    Nouvelle interface graphique

---
+++ src/main/java/ihm/Main.java
@@ -0,0 +1,13 @@
+package ihm;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Window window = Window.getInstance();
+		
+		FrameConnection connection = new FrameConnection();
+		connection.setVisible(true);
+		
+	}
+}
