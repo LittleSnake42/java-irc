a07faed2777165f1436fba335c2b890d4585aefc
Rémi Marandon <rmarandon@axome.com>
2017-04-04 14:06:55 +0200

    [not tested] Nouvelle classe et thread pour lire dans le buffer

--- src/main/java/utils/ClientListener.java
+++ src/main/java/utils/ClientListener.java
@@ -2,20 +2,20 @@ package utils;
 
 import serial.MessageFromServer;
 
-public class ClientListener implements Runnable{
+public class ClientListener implements Runnable {
 
 	public ClientListener() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public void run() {
-		
+
 		MessageControler mc = MessageControler.getInstance();
-		
+
 		MessageFromServer msg = null;
-		
-		while(true){
-			
+
+		while (true) {
+
 			try {
 				msg = mc.read();
 				mc.processServerMessage(msg);
@@ -24,11 +24,9 @@ public class ClientListener implements Runnable{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
+
 		}
-		
-		
+
 	}
 
 }
